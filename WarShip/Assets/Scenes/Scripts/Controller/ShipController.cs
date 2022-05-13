24310656bbb720cba12b7e7ba3329eb2e2359242
NCC1701-J <fangji.qian@students.plymouth.ac.uk>
2022-05-13 11:52:42 +0800

    Configuration Environment

---
+++ WarShip/Assets/Scenes/Scripts/Controller/ShipController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour
+{
+    bool IsDead = false;
+
+
+    protected Move movement;
+
+
+    protected virtual void Start()
+    {
+        movement = transform.GetComponent<Move>();
+    }
+
+    protected virtual void Update()
+    {
+
+    }
+
+    protected virtual void FixedUpdate()
+    {
+        if (IsDead) { return; }
+        ControlUpdate();
+    }
+
+    protected virtual void ControlUpdate()
+    {
+
+    }
+}
