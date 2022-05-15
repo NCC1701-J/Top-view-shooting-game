5b818eebee9ddfd4d074333d9ca78e0ac70dde81
NCC1701-J <fangji.qian@students.plymouth.ac.uk>
2022-05-15 12:03:56 +0800

    Alpha 0.1
    In the Alpha 0.1 version, public sea materials, ship models, and some other required materials have been added. Also, added ship movement and camera following scripts to test whether the player can control the ship in the scene.

---
+++ WarShip/Assets/Scenes/Scripts/Controller/CharacherController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacherController : ShipController
+{
+    public Camera cam;
+
+    protected override void ControlUpdate()
+    {
+        movement.speedUp = Input.GetKey(KeyCode.W);
+        movement.turnLeft = Input.GetKey(KeyCode.A);
+        movement.turnRight = Input.GetKey(KeyCode.D);
+    }
+}
