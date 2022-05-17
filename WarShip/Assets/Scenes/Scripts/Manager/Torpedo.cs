ffc398bcf78517a233fbaac4cdb115511f6012a2
NCC1701-J <fangji.qian@students.plymouth.ac.uk>
2022-05-17 16:50:05 +0800

    Alpha 0.6
    Added a new weapon - torpedo. Added models of torpedo bases, torpedo tubes and torpedoes. As well as the particle effect after the torpedo hits the target. Also added scripts related to them.

---
+++ WarShip/Assets/Scenes/Scripts/Manager/Torpedo.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Torpedo : Projectile
+{
+
+    public float velocityDecay;
+
+    Transform sea;
+
+    protected override void Start()
+    {
+        base.Start();
+        sea = GameObject.Find("Sea").transform;
+    }
+
+    protected override void FixedUpdate()
+    {
+        base.FixedUpdate();     
+    }
+
+    protected override void MoveLogicUpdate()
+    {
+        speed -= speed < 0 ? 0 : velocityDecay * Time.fixedDeltaTime;
+        distance = speed * Time.fixedDeltaTime;
+        transform.position += direction * distance;
+    }
+
+    protected override void PlayParticleAtPoint(ParticleSystem pc, Vector3 point, Vector3 direction)
+    {
+        //Debug.Log(new Vector3(point.x, 0, point.z)+":"+point);
+        if(point!=Vector3.zero)
+        {
+            pc.transform.position = new Vector3(point.x, 0, point.z);
+        }
+        else
+        {
+            pc.transform.position = transform.position;
+        }
+        pc.transform.rotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        //Set the trigger for the splash hit the sea surface
+        pc.transform.GetChild(0).GetComponent<ParticleSystem>().trigger.SetCollider(0, sea);   
+        pc.Play();
+    }
+}
---
+++ WarShip/Assets/Scenes/Scripts/weapons/TorpedoTube.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorpedoTube : Emitter
+{
+
+    public float SpreadAngle;
+
+    public int TotalNumber;
+
+    Vector3[] Directions;
+
+    protected override void Start()
+    {
+        base.Start();
+
+        Directions = new Vector3[TotalNumber];
+    }
+
+
+    protected override void Update()
+    {
+
+    }
+
+    protected override void Shoot()
+    { 
+        RaycastHit hit;
+        GetDirections(SpreadAngle, TotalNumber);
+        int layerMask =1<< LayerMask.NameToLayer("Water");
+        
+        if (Physics.Raycast(muzzle.position, -Vector3.up, out hit, 10f, ~layerMask))
+        {    
+            for(int i=0;i<Directions.Length;i++)
+            {
+                Instantiate(projectile).GetComponent<Projectile>().Init(new Vector3(hit.point.x,hit.point.y-0.55f,hit.point.z), Directions[i], speed, lifeTime, damage,this.gameObject);
+            }          
+        }
+        else
+        {
+            Debug.Log("layerError");
+        }
+        //Debug.DrawRay(muzzle.position, -Vector3.up * 10, Color.red, 10f);
+    }
+
+    void GetDirections(float spreadAngle, int num)
+    {
+        Vector3 baseDir = Quaternion.AngleAxis(-spreadAngle / 2, Vector3.up) * new Vector3(muzzle.forward.x, 0, muzzle.forward.z);
+        float eachAngle = spreadAngle / num;
+        for (int i = 0; i < TotalNumber; i++)
+        {
+            Directions[i] = Quaternion.AngleAxis(i * eachAngle, Vector3.up) * baseDir;
+        }
+    }
+}
